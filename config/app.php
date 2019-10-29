2da1bd4289c1761e215f5bfa3a625611e023933d
3a632081 <s3a632081@student.ncut.edu.tw>
2019-10-29 12:43:23 +0800

    練習1:註冊 Service Provider

--- config/app.php
+++ config/app.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 return [
 
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Recca0120\LaravelTracy\ServiceProvider::class,
 
         /*
          * Package Service Providers...
